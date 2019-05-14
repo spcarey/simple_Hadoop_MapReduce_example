d8a884a9fb39531768fafca323e70863ed79e8ad
spcarey <sean.carey@careys.io>
2019-05-14 18:04:53 +0000

    lowercased, removed stopwords, removed punctuation

--- mapper.py
+++ mapper.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 import sys
+from sklearn.feature_extraction import stop_words
+import string
+# create set of stopworkds
+
+stops = set(stop_words.ENGLISH_STOP_WORDS)
+
+# Remove stopwrods using loop
+
+
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
+
+    # remove punctuation
+    line =  line.translate(None, string.punctuation)
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
+      if word not in stops:
         print '%s\t%s' % (word, "1")
+
