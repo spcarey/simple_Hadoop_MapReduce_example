e9c404454e4ac830270b3b51c4663c106a5623f0
spcarey <sean.carey@careys.io>
2019-05-15 15:46:20 +0000

    added stopwords from nltk and spacy, adding spacy breaks the mr job but works in command line

--- mapper.py
+++ mapper.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 import sys
 from sklearn.feature_extraction import stop_words
+import functools
+from operator import or_
+import spacy as spacy 
+import nltk
+from nltk.corpus import stopwords 
 import string
 # create set of stopworkds
 
-stops = set(stop_words.ENGLISH_STOP_WORDS)
+# Load spacy stopwords
 
+
+spacy_nlp = spacy.load('en')
+
+spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+stop1 = set(spacy_stopwords)
+stop1 = set([x.encode('utf-8') for x in stop1])
+
+
+# load  sklearn stopwords
+stop2 = set(stop_words.ENGLISH_STOP_WORDS)
+
+# load nltk stopwords
+stop3 = set(stopwords.words('english'))
+stop3 = set([x.encode('utf-8') for x in stop3])
+
+
+
+
+stops = reduce(or_ , [ stop2, stop3])
 # Remove stopwrods using loop
 
 
@@ -23,6 +47,6 @@ for line in sys.stdin:
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-      if word not in stops:
+      if word not in  stops:
         print '%s\t%s' % (word, "1")
 
