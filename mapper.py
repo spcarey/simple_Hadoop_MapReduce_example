899a8f4683935f55bd23a460e472d8a8d9d41f80
spcarey <sean.carey@careys.io>
2019-05-15 15:13:40 -0400

    added stop words from sklearn

--- mapper.py
+++ mapper.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 import sys
 import string
+from sklearn.feature_extraction import stop_words
+import functools
+from operator import or_
+import spacy as spacy
+import nltk
+from nltk.corpus import stopwords 
+
+# create set of stopworkds
+
+# Load spacy stopwords
+
+#spacy_nlp = spacy.load('en_core_web_sm')
+#spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+#stop1 = set(spacy_stopwords)
+#stop1 = set([x.encode('utf-8') for x in stop1])
+
+# load  sklearn stopwords
+
+stops = set(stop_words.ENGLISH_STOP_WORDS)
+
+# load nltk stopwords
+#nltk.download('stopwords')
+#stop3 = set(stopwords.words('english'))
+#stop3 = set([x.encode('utf-8') for x in stop3])
+
+# combine all stopwords  from each set
+
+#stops = functools.reduce(or_, [ stop2, stop3])
+
+# stops =
+
+# Remove stopwrods using loop
 
 # get all lines from stdin
 for line in sys.stdin:
@@ -16,4 +48,5 @@ for line in sys.stdin:
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print ('%s\t%s' % (word, "1"))
+        if word not in stops:
+          print ('%s\t%s' % (word, "1"))
