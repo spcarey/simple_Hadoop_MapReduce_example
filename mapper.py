1cb8befc2e3fde46e5f7d8092ddc094f18bedbcf
spcarey <sean.carey@careys.io>
2019-05-15 19:08:40 +0000

    added python3 to #! line, removed punctuation

--- mapper.py
+++ mapper.py
@@ -1,10 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
+import string
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
+
+    # remove punctuation
+    #table = str.maketrans({key: None for key in string.punctuation})
+    line = line.translate(str.maketrans("","", string.punctuation))
 
     # split the line into words; splits on any whitespace
     words = line.split()
